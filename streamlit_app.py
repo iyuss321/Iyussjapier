1d740424324531ab3aaf785f14bd11465c773ef3
iyuss321 <fairuzjavierghazalah@gmail.com>
2025-05-27 07:53:05 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.write(
 st.image("IMG-20250515-WA0045.jpeg", width=500) 
 
 st.title("IYUSS GAMESS") 
-st.header("Aplikasi Pengecek Nilai Ganjil Dan Genap")
+st.header("Mengecek Bilangan Ganjil Dan Genap")
 angka = st.number_input("Tulis Sebuah Angka:", value= 0, step= 1) 
 
 
